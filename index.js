b2ecbc08cf3306699d582ef19f96c8acb1edb498
Kyla Quilos <quilos.kyla@gmail.com>
2021-09-29 13:17:15 -0700

    created Express code to route all endpoints

--- index.js
+++ index.js
@@ -55,6 +55,38 @@ app.get('/', (req, res) => {
 	res.send('Watching movies is definitely one of my hobbies!');
 });
 
+app.get('/movies/:title', (req, res) => {
+	res.send('Successful GET request returning data on a single movie by title to the user.');
+});
+
+app.get('/genres/:name', (req, res) => {
+	res.send('Successful GET request returning data on a genre by name.');
+});
+
+app.get('/directors/:name', (req, res) => {
+	res.send('Successful GET request returning data a director by name.');
+});
+
+app.post('/users/register', (req, res) => {
+	res.send('Successful POST request registering a new user.');
+});
+
+app.put('/users/:username/edit_profile', (req, res) => {
+	res.send('Successful PUT request updating a user\'s profile.');
+});
+
+app.post('/users/:username/favorites', (req, res) => {
+	res.send('Successful POST request adding a movie to a user\'s favorites.');
+});
+
+app.delete('/users/:username/favorites', (req, res) => {
+	res.send('Successful DELETE request returning data on a genre by name.');
+});
+
+app.delete('/users', (req, res) => {
+	res.send('Successful DELETE request returning data on a genre by name.');
+});
+
 app.use(express.static('public'));
 
 app.use((err, req, res, next) => {
