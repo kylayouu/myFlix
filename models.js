14181d0809cd24e6895147c4c4ece6124fa6e8a2
Kyla Quilos <quilos.kyla@gmail.com>
2021-10-14 15:27:11 -0700

    updated attributes

--- models.js
+++ models.js
@@ -9,9 +9,9 @@ let movieSchema = mongoose.Schema({
 	},
 	Director: {
 		Name: String,
-		Bio: String
+		Bio: String,
+		Birth: String
 	},
-	Actors: [String],
 	ImagePath: String,
 	Featured: Boolean
 });
