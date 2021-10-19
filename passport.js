ab83b3cec86842cb3803a66326e5a7cb26a508e4
Kyla Quilos <quilos.kyla@gmail.com>
2021-10-19 15:30:19 -0700

    fixed typos

--- passport.js
+++ passport.js
@@ -28,8 +28,8 @@ passport.use(new LocalStrategy({
 	});
 }));
 
-passport.user(new JWTStrategy({
-	jwtFromRequest: ExtractJWT.fromtAuthHeaderAsBearerToken(),
+passport.use(new JWTStrategy({
+	jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
 	secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
 	return Users.findById(jwtPayload._id)
