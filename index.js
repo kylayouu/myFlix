c73e46518ac33bc5d6451157a9655d3ee18c321d
Kyla Quilos <quilos.kyla@gmail.com>
2021-10-21 16:35:41 -0700

    hashing and validating passwords

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -71,31 +72,42 @@ app.get('/directors/:Name', (req, res) => {
   });
 });
 
-app.post('/users/register', (req, res) => {
-	Users.findOne( {Username: req.body.Username} ).then((user) => {
-    if (user) {
-      return res.status(400).send(req.body.Username + ' already exists');
-    } else {
-        Users.create({
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday,
-          FavoriteMovies: req.body.FavoriteMovies
-        })
-        .then((user) => {
-          res.status(201).json(user)
-        })
-        .catch((err) => {
-          console.error(err);
-          res.status(500).send('Error: ' + err);
-        })
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
+app.post('/users/register', [
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne( {Username: req.body.Username} ).then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday,
+            FavoriteMovies: req.body.FavoriteMovies
+          })
+          .then((user) => {
+            res.status(201).json(user)
+          })
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+          })
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
 });
 
 app.put('/users/:Username', (req, res) => {
--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
 	Title: {type: String, required: true},
@@ -24,6 +25,16 @@ let userSchema = mongoose.Schema({
 	Birthday: Date,
 	FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
+
+// hashes the password
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+// compares hashed password to stored hashed password
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let genreSchema = mongoose.Schema({
 	Name: {type: String, required: true},
