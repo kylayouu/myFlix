8715faabb1af0eb911606459e65132fccf36e17a
Kyla Quilos <quilos.kyla@gmail.com>
2021-12-09 16:35:42 -0800

    added get request for users

--- index.js
+++ index.js
@@ -47,6 +47,16 @@ app.get('/movies', passport.authenticate('jwt', { session: false }),(req, res) =
   });
 });
 
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username }).then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
+
 app.get('/movies/:Title', (req, res) => {
 	Movies.findOne({Title: req.params.Title}).then((movie) => {
     res.json(movie);
